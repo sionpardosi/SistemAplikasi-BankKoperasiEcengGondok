c597036766ffbc87c2b289daf8aea8247ee042b1
sionprdsi <spardosi12@gmail.com>
2025-02-20 13:41:15 +0700

    Developing a website with product features, product details, and authentication (MVP).

--- bank-koperasi-eceng-gondok/routes/web.php
+++ bank-koperasi-eceng-gondok/routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Middleware\AuthAdmin;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Auth::routes();
+
+// index Home
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+// User
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard',[UserController::class,'index'])->name('user.index');
 });
+//  Admin
+Route::middleware(['auth',AuthAdmin::class])->group(function(){
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+});
+
+// Route::middleware([AuthUser::class])->group(function(){
+//     Route::get('/account-dashboard',[UserController::class,'account_dashboard'])->name('user.account.dashboard');
+// });
+// Route::middleware([AuthAdmin::class])->group(function(){
+//     Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+// });
