9a49fa703d7c213bda01a26ab8f49e14304b545d
sionprdsi <spardosi12@gmail.com>
2025-05-18 09:15:24 +0700

    Middleware untuk Validasi reCAPTCHA

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,8 +20,13 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
+
+    protected $routeMiddleware = [
+        // Middleware lain yang sudah ada
+        'recaptcha' => \App\Http\Middleware\VerifyRecaptcha::class,
+    ];
 }
